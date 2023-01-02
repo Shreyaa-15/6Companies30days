e8a970b905c768364003fe3a7a514018a898fdf6
Shreya <86222020+Shreyaa-15@users.noreply.github.com>
2023-01-03 00:06:02 +0530

    Create sum_comb.cpp

---
+++ Microsoft/sum_comb.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> arr;
+        dfs(k,n,1, arr,ans);
+        return ans;
+    }
+    void dfs(int k, int n, int s, vector<int> &arr, vector<vector<int>> &ans){
+        if(k == 0 && n == 0)
+        {
+            ans.push_back(arr);
+            return;
+        }
+        if(k == 0 || n <= 0)
+            return;
+        for(int i = s; i <= 9; i++){
+            arr.push_back(i);
+            dfs(k-1,n-i,i+1,arr,ans);
+            arr.pop_back();
+        }
+    }
+};
+
+//Run time = 0ms
+// Memory = 6.3MB
