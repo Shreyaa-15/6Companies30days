3ba188a909d29f58f9b94a76ba27795e7bbc5996
Shreya <86222020+Shreyaa-15@users.noreply.github.com>
2023-01-05 10:01:30 +0530

    Create ArrayDivisible.cpp

---
+++ Microsoft/ArrayDivisible.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, vector<int>& numsDivide) {
+        int ans = -1;
+        int n = nums.size();
+       sort(nums.begin() , nums.end());
+        int gcdd = *max_element(numsDivide.begin() , numsDivide.end());
+        for(auto i:numsDivide)
+        {
+            gcdd = __gcd(gcdd , i);
+        }
+        cout<<gcdd<<"\n";
+        for(int i=0;i<n;++i)
+        {
+            if(gcdd%nums[i] == 0)
+            {
+               ans = i;
+                break;
+            } 
+        }
+        return ans;
+    }
+};
+
+//Run time = 202ms
+//Memory = 88.2MB
