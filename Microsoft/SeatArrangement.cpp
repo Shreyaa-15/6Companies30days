46f3080db410c7df95840f0f5ffa2adfb460af90
Shreya <86222020+Shreyaa-15@users.noreply.github.com>
2023-01-05 09:57:11 +0530

    Create SeatArrangement.cpp

---
+++ Microsoft/SeatArrangement.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        if(n==1) return (double)n;
+       else return (double)1/2; 
+    }
+};
+
+//Run Time = 3ms
+//Memory = 6.1MB
